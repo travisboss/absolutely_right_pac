3d3fd81491e099c301f7cf56b112a8b149d3a3da
Travis Boss <travisboss@gmail.com>
2021-02-17 20:48:25 +0000

    remove prefix

--- config.js
+++ config.js
@@ -7,5 +7,4 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: 'https://absolutelyright.org/', // This path is subpath of your hosting https://domain/portfolio
 };

531ef9f35a4693f5a7b3905f89658b3949bc2494
Travis Boss <travisboss@gmail.com>
2021-02-17 16:05:56 +0000

    updated websiteicon

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ module.exports = {
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  // manifestIcon: 'src/assets/img/website-icon.png',
+  manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
 };

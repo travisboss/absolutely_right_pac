254aca7358d5f2fa616bdf437c0f82aace6cce4e
Travis Boss <travisboss@gmail.com>
2021-02-17 20:44:27 +0000

    add url prefix

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/absolutely_right_pac/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: 'https://absolutelyright.org/', // This path is subpath of your hosting https://domain/portfolio
 };
